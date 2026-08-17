dfa096933bf887e4ad48cd8e17c2a6d61046e209
anupamkedia <anupamkedia@gmail.com>
2026-08-17 20:05:18 +0530

    Fix supabase service client - final

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -9,7 +9,7 @@ export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 export function createServiceClient() {
   return createClient(
     (process.env.NEXT_PUBLIC_SUPABASE_URL || ""),
-    process.env.SUPABASE_SERVICE_ROLE_KEY!,
+    (process.env.SUPABASE_SERVICE_ROLE_KEY || ""),
     { auth: { autoRefreshToken: false, persistSession: false } }
   );
 }
